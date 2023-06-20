2b26435503c565682808d41c3cc1f815ee48a795
Elineum <you@example.com>
2023-06-20 04:40:40 +0300

    added 12hw

---
+++ main.js
@@ -0,0 +1,18 @@
+const closureSum = () => {
+  let rememberedVariable = 0;
+
+  return (num) => {
+    if (typeof num !== "number" || Number.isNaN(num))
+      return "Entered wrong value";
+
+    return (rememberedVariable += num);
+  };
+};
+
+const sum = closureSum();
+
+console.log(sum(3));
+console.log(sum(6));
+console.log(sum(NaN));
+console.log(sum(62));
+console.log(sum(6865));
